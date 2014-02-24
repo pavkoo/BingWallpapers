afc3ba1e14ef12b9c30b8e44dbe321731f700b34
pavkoo <pavkoo@live.com>
2014-02-24 23:46:15 +0800

    ..

--- src/ProjectCode/Main.pas
+++ src/ProjectCode/Main.pas
@@ -27,6 +27,10 @@ type
     InvertEffect1: TInvertEffect;
     bottomShadow: TRectangle;
     FloatAnimation: TFloatAnimation;
+    GaussianBlurEffect: TGaussianBlurEffect;
+    ActionUp: TAction;
+    ActionDown: TAction;
+    FloatAnimation1: TFloatAnimation;
     procedure FormCreate(Sender: TObject);
     procedure lyImageInfoMouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Single);
@@ -35,10 +39,13 @@ type
     procedure lyImageInfoMouseMove(Sender: TObject; Shift: TShiftState; X,
       Y: Single);
     procedure FormShow(Sender: TObject);
+    procedure ActionUpExecute(Sender: TObject);
+    procedure ActionDownExecute(Sender: TObject);
   private
     { Private declarations }
     FCurrentUIStyle:TUIStyle;
     ISmouseDown:Boolean;
+    FMouseDownPoint:TPointf;
     procedure SetCurrentUIStyle(const Value:TUIStyle);
     procedure ComponentEnter(const Value: TUIStyle);
     procedure ComponentQuit(const Value: TUIStyle);
@@ -52,11 +59,22 @@ var
 
 const
   ABSPOSITIONYIMAGEINFO = 120;
+  AniDuiation = 0.5;
 
 implementation
 {$R *.fmx}
 
 { TfrmMain }
+
+procedure TfrmMain.ActionDownExecute(Sender: TObject);
+begin
+  CurrentUIStyle := TUIMain;
+end;
+
+procedure TfrmMain.ActionUpExecute(Sender: TObject);
+begin
+  CurrentUIStyle := TUIDetail;
+end;
 
 procedure TfrmMain.ComponentEnter(const Value: TUIStyle);
 begin
@@ -69,19 +87,32 @@ begin
       end;
      TUIMain:
       begin
+        bottomShadow.Opacity := 0.8;
+        GaussianBlurEffect.BlurAmount :=  0.01 ;
+        if assigned(FloatAnimation) then FloatAnimation.Free;
+        FloatAnimation := TFloatAnimation.Create(nil);
+        FloatAnimation.Parent :=  lyImageInfo;
         lyImageInfo.Visible := True;
         lyImageInfo.Position.Y := Self.Height;
         FloatAnimation.PropertyName := 'Position.Y';
-        FloatAnimation.Duration := 1;
+        FloatAnimation.Duration := AniDuiation;
         FloatAnimation.Delay := 0.5;
         FloatAnimation.Interpolation := TInterpolationType.itLinear;
         FloatAnimation.StartValue := Self.Height;
-        FloatAnimation.StopValue := Self.Height -ABSPOSITIONYIMAGEINFO;
-        FloatAnimation.Parent :=  lyImageInfo;
+        FloatAnimation.StopValue := Self.Height - ABSPOSITIONYIMAGEINFO;
         FloatAnimation.Start;
       end;
      TUIDetail:
       begin
+        if assigned(FloatAnimation) then FloatAnimation.Free;
+        FloatAnimation := TFloatAnimation.Create(nil);
+        FloatAnimation.Parent :=  lyImageInfo;
+        FloatAnimation.PropertyName := 'Height';
+        FloatAnimation.Duration := AniDuiation;
+        FloatAnimation.Interpolation := TInterpolationType.itLinear;
+        FloatAnimation.StartFromCurrent := True;
+        FloatAnimation.StopValue := Self.Height- ABSPOSITIONYIMAGEINFO;
+        FloatAnimation.Start;
       end;
      TUICountry:
       begin
@@ -103,18 +134,18 @@ begin
       end;
      TUIMain:
       begin
-        lyImageInfo.Visible := True;
-        lyImageInfo.Position.Y := Self.Height;
-        FloatAnimation.PropertyName := 'Position.Y';
-        FloatAnimation.Duration := 1;
-        FloatAnimation.Interpolation := TInterpolationType.itLinear;
-        FloatAnimation.StartValue := lyImageInfo.Position.Y + ABSPOSITIONYIMAGEINFO;
-        FloatAnimation.StopValue := Self.Height;
-        FloatAnimation.Parent :=  lyImageInfo;
-        FloatAnimation.Start;
       end;
      TUIDetail:
       begin
+        if assigned(FloatAnimation) then FloatAnimation.Free;
+        FloatAnimation := TFloatAnimation.Create(nil);
+        FloatAnimation.Parent :=  lyImageInfo;
+        FloatAnimation.PropertyName := 'Height';
+        FloatAnimation.Duration := AniDuiation;
+        FloatAnimation.Interpolation := TInterpolationType.itLinear;
+        FloatAnimation.StartValue := Self.Height;
+        FloatAnimation.StopValue := ABSPOSITIONYIMAGEINFO;
+        FloatAnimation.Start;
       end;
      TUICountry:
       begin
@@ -141,29 +172,28 @@ procedure TfrmMain.lyImageInfoMouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Single);
 begin
   if Button = TMouseButton.mbLeft then
+  begin
     ISmouseDown:= True;
+    FMouseDownPoint.X := X;
+    FMouseDownPoint.Y := Y;
+    bottomShadow.Opacity := 0.4;
+  end;
 end;
 
 procedure TfrmMain.lyImageInfoMouseMove(Sender: TObject; Shift: TShiftState; X,
   Y: Single);
 begin
   if ISmouseDown then
+  begin
     lyImageInfo.Height := lyImageInfo.Height + (FMouseDownPoint.Y-y);
+    GaussianBlurEffect.BlurAmount :=  0.01 + (Self.Height - lyImageInfo.Position.Y) / Self.Height;
+  end;
 end;
 
 procedure TfrmMain.lyImageInfoMouseUp(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Single);
 begin
   ISmouseDown := False;
-  //should show detail
-  if lyImageInfo.Position.Y < ABSPOSITIONYIMAGEINFO then
-  begin
-    CurrentUIStyle := TUIDetail;
-  end
-  //hideDetail
-  else
-  begin
-  end;
 end;
 
 procedure TfrmMain.SetCurrentUIStyle(const Value: TUIStyle);
